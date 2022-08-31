d1e8d81341a840ae547919a9ce4467c6931c596d
xaalps <xahuiwb@hotmail.com>
2022-08-31 10:04:45 +0800

    Full pipeline test20220831

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello My New World! \n ANYONE HERE WANT EAT? \n 20220830 '));
+app.get('/', (req, res) => res.send('Hello My New World! \n <br> ANYONE HERE WANT EAT? 有人吃饭么？\n <br> 20220831宣 '));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
